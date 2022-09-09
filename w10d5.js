580b63a5712799634b1507cedde29c819d2aa822
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-09 08:40:03 +0100

    first commit

---
+++ w10d5.js
@@ -0,0 +1,61 @@
+//NO PHONE, NO EMAILS, NO COPY AND PASTING!!!
+​
+//write a for loop that counts from 0 to 10 in the console
+
+for (let i = 0; i < 11; i++) {
+console.log(i);
+}
+
+// write a for loop that counts from 0 to 20 in the console.
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+​}
+// write a for loop that counts from 10 to 0 in the console.
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+​}
+// write a for loop that counts from 20 to 0 in the console.
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+​}
+//write a for loop that counts from 0 to 10 by 2's in the console (0,2,4,6,8,10)
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+​}
+//write a for loop that counts from 10 to 0 in the console.
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+​}
+//=============================================================================================================
+//=============================================================================================================
+//=============================================================================================================
+​
+// write a function that adds two numbers.  call the function in a console log.
+​
+function adds(x, y) {
+return x + y;
+}
+console.log(adds(2, 4));
+​
+//write a function that doubles a number
+​function doubler(x, y) {
+    return x + y;
+    }
+    console.log(doubler(2, 4));
+// write a function that adds three numbers
+function addsThree(x, y, z) {
+    return x + y + z;
+    }
+    console.log(adds(2, 4, 6));
+    ​
+//write a function that multiplies a number by 10
+​function multiplier(x, y, z) {
+    return x + y + z;
+    }
+    console.log(adds(2, 4, 6));
+//write a function that adds an exclaimation mark to a string
+function shocked("Shocked") {
+    return
+    }
+    console.log(shocked);
+//write a function that joins two strings together.

e0426c9e435964c18803320bf8865762f40ae1df
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-09 08:46:15 +0100

    updated

--- w10d5.js
+++ w10d5.js
@@ -7,22 +7,27 @@ console.log(i);
 }
 
 // write a for loop that counts from 0 to 20 in the console.
+
 for (let i = 0; i < 11; i++) {
     console.log(i);
 ​}
 // write a for loop that counts from 10 to 0 in the console.
+
 for (let i = 0; i < 11; i++) {
     console.log(i);
 ​}
 // write a for loop that counts from 20 to 0 in the console.
+
 for (let i = 0; i < 11; i++) {
     console.log(i);
 ​}
 //write a for loop that counts from 0 to 10 by 2's in the console (0,2,4,6,8,10)
+
 for (let i = 0; i < 11; i++) {
     console.log(i);
 ​}
 //write a for loop that counts from 10 to 0 in the console.
+
 for (let i = 0; i < 11; i++) {
     console.log(i);
 ​}
@@ -38,23 +43,28 @@ return x + y;
 console.log(adds(2, 4));
 ​
 //write a function that doubles a number
-​function doubler(x, y) {
-    return x + y;
+
+​function doubler(x) {
+    return x * 2;
     }
-    console.log(doubler(2, 4));
+    console.log(doubler(2));
 // write a function that adds three numbers
+
 function addsThree(x, y, z) {
     return x + y + z;
     }
-    console.log(adds(2, 4, 6));
+    console.log(addsThree(2, 4, 6));
     ​
 //write a function that multiplies a number by 10
+
 ​function multiplier(x, y, z) {
-    return x + y + z;
+    return (x * y) * z;
     }
     console.log(adds(2, 4, 6));
 //write a function that adds an exclaimation mark to a string
-function shocked("Shocked") {
+
+function shocked("") {
+
     return
     }
     console.log(shocked);

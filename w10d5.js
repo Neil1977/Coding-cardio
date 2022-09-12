69937e90dc001c980ecac924182b3bc5d926f03e
Neil1977 <Neil_lunn1977@hotmail.com>
2022-09-12 09:38:06 +0100

    W11D1

--- w10d5.js
+++ w10d5.js
@@ -68,3 +68,52 @@ function joiner(a, b) {
     return (str.concat('', ''));
 }
 console.log(joiner(a,b))
+
+//NO PHONE, NO EMAILS, NO COPY AND PASTING!!!
+
+//write a for loop that counts from 0 to 10 in the console
+​for (let i = 0; i < 11; i++) {
+    console.log(i);
+    }
+
+//write a for loop that counts from 10 to 0 in the console
+​
+for (let i = 11; i > 11; i--) {
+console.log(i);
+}
+​
+//=============================================================================================================
+//=============================================================================================================
+//=============================================================================================================
+​
+// write a function that adds two numbers.  call the function in a console log.
+​
+function adds(x, y) {
+return x + y;
+}
+console.log(adds(2, 4));
+​
+//write a function that triples a number
+function tripler(x) {
+return x * 3;
+}
+console.log (tripler(2));
+
+// write a function that adds three numbers
+function addThree(x, y, z) {
+    return x + y + z;
+}
+console.log (addThree(2, 4, 6));
+​
+//write a function that joins two strings together.
+function plusStrings()
+return (str.concat('', ''));
+console.log(plusStrings("dog", "cat"));
+​
+//write a function that accepts an array and logs its values out.  (combining function and for-loop)
+function arrayLogger(numbers) {
+    let numbers = [1, 2, 3, 4];
+    for (let i = 0; i < numbers.length; i++)
+    console.log(arrayLogger[i]);
+}
+
